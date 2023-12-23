cbc73d1334e56a09e3ad72b3e9f54e206717f69b
Fathurrahman <41844599+fathuurr@users.noreply.github.com>
2023-12-23 22:39:45 +0700

    fix responsive mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState('Ideas');
 
   useEffect(() => {
     const handleScroll = () => {
@@ -19,8 +23,6 @@ const Navbar = () => {
 
     return () => window.removeEventListener('scroll', handleScroll);
   }, [prevScrollPos, visible]);
-
-  const [activeMenu, setActiveMenu] = useState('Ideas');
 
   return (
     <nav
@@ -85,6 +87,86 @@ const Navbar = () => {
               Contact
             </li>
           </ul>
+          <div
+            className='hidden max-lg:block cursor-pointer'
+            onClick={() => {
+              setIsMenuOpen(!isMenuOpen);
+            }}>
+            <RxHamburgerMenu className='text-4xl' />
+          </div>
+
+          {isMenuOpen && (
+            <div>
+              <nav className='fixed top-0 right-0 left-0 bottom-0 lg:bottom-auto bg-orange-400  '>
+                <div
+                  className='hidden max-lg:block fixed right-0  px-8 py-4 cursor-pointer'
+                  onClick={() => {
+                    setIsMenuOpen(!isMenuOpen);
+                  }}>
+                  <AiOutlineClose className='text-4xl' />
+                </div>
+                <ul className=' lg:hidden flex flex-col items-center justify-center h-full '>
+                  <li
+                    className={` cursor-pointer ${
+                      activeMenu === 'Work'
+                        ? 'text-white border-b-2'
+                        : 'text-white'
+                    }`}
+                    onClick={() => setActiveMenu('Work')}>
+                    Work
+                  </li>
+                  <li
+                    className={` cursor-pointer ${
+                      activeMenu === 'About'
+                        ? 'text-white border-b-2'
+                        : 'text-white'
+                    }`}
+                    onClick={() => setActiveMenu('About')}>
+                    About
+                  </li>
+
+                  <li
+                    className={` cursor-pointer ${
+                      activeMenu === 'Services'
+                        ? 'text-white border-b-2'
+                        : 'text-white'
+                    }`}
+                    onClick={() => setActiveMenu('Services')}>
+                    Services
+                  </li>
+
+                  <li
+                    className={` cursor-pointer ${
+                      activeMenu === 'Ideas'
+                        ? 'text-white border-b-2'
+                        : 'text-white'
+                    }`}
+                    onClick={() => setActiveMenu('Ideas')}>
+                    Ideas
+                  </li>
+
+                  <li
+                    className={` cursor-pointer ${
+                      activeMenu === 'Services'
+                        ? 'text-white border-b-2'
+                        : 'text-white'
+                    }`}
+                    onClick={() => setActiveMenu('Services')}>
+                    Careers
+                  </li>
+                  <li
+                    className={`cursor-pointer ${
+                      activeMenu === 'Contact'
+                        ? 'text-white border-b-2'
+                        : 'text-white'
+                    }`}
+                    onClick={() => setActiveMenu('Contact')}>
+                    Contact
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          )}
         </div>
       </div>
     </nav>
